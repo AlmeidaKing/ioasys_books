341afc11ac182e9e78a0e18ecc84d02b732def3c
AlmeidaKing <almeida_jn@hotmail.com>
2021-11-12 19:47:00 -0300

    chore: configuring lint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
